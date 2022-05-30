ed7a0ab1813e94b60c266c4d220e9d8c7c221aaa
norcimilim.x <norcimilim@norcimilim.x>
2022-05-31 02:05:33 +0300

    Add winner section, add surrender and restart methods, add health watchers

--- app.js
+++ app.js
@@ -7,21 +7,50 @@ const app = Vue.createApp({
         return{
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         };
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return {width: '0%'};
+            }
             return {width: this.monsterHealth + '%'};
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return {width: '0%'};
+            }
             return {width: this.playerHealth + '%'};
         },
         mayUseSpecialAttack() {
             return this.currentRound % 3 !== 0;
         }
     },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
+    },
     methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+        },
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
@@ -48,6 +77,9 @@ const app = Vue.createApp({
             }
             this.attackPlayer();
         },
+        surrender() {
+            this.winner = 'monster';
+        }
     }
 });
 
